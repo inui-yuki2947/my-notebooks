ff8b22dedf23bbd193b3c6902c446e3d8d4d8ab2
Inui Yuki <60595603+inui-yuki2947@users.noreply.github.com>
2021-12-26 14:50:59 +0900

    フォーマットスクリプトで、ファイル名を変数に収めるよう変更

--- scripts/h1-format-check.sh
+++ scripts/h1-format-check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # 引数のファイルについて、1行目が「# ファイル名（先頭の番号を除く）」になっているかチェックする。
-find . -name "*.md" -not -name "README.md" | while read -r file; do
+files=$(find . -name "*.md" -not -name "README.md")
+while read -r file; do
   title=$(basename "$file" .md | sed "s/^[0-9]*_/# /")
   h1=$(head -n 1 "$file")
   if [[ "$h1" != "$title" ]]; then
       echo "error in $file: \"$h1\" should be \"$title\"" >&2
       exit 1
   fi
-done
+done <<< "$files"
