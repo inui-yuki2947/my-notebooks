156a9dacb97ebd7b627d32d6716f0bb5d5f3b522
Inui Yuki <60595603+inui-yuki2947@users.noreply.github.com>
2021-12-27 14:09:23 +0900

    フォーマットチェックスクリプトを、別リポジトリに移動

--- scripts/h1-format-fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 引数のファイルについて、1行目を「# ファイル名（先頭の番号を除く）」に変更する。
-files=$(find . -name "*.md" -not -name "README.md")
-while read -r file; do
-  title=$(basename "$file" .md | sed "s/^[0-9]*_//")
-  gsed -i "1c# $title" "$file"
-done <<< "$files"
