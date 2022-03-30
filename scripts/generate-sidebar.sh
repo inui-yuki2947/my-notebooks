7e3dd52083ba8a9d1f058e933dcd2c89e0557af0
Inui Yuki <60595603+inui-yuki2947@users.noreply.github.com>
2022-03-30 16:34:54 +0900

    サイドバーで、リンク内にスペースが含まれる場合に対応

--- scripts/generate-sidebar.sh
+++ scripts/generate-sidebar.sh
@@ -3,5 +3,5 @@ set -e
 
 # 現在のファイル構成をもとに、サイドバー用のmdファイルを生成する。
 cd "$(dirname "$0")/../docs"
-content=$(find . -type f -name "*.md" | sort | while read -r path; do echo "- [${path}](${path})"; done)
+content=$(find . -type f -name "*.md" | sort | while read -r path; do echo "- [${path}](<${path}>)"; done)
 (echo "- [Home](/)"; echo "$content") > _sidebar.md
