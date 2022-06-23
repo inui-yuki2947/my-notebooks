271ef5cbeb6c9f75010d4fe28cc7a27af1390143
Inui Yuki <60595603+inui-yuki2947@users.noreply.github.com>
2022-06-23 15:27:15 +0900

    reformatスクリプトについて、連番の修正のみ行うよう変更

--- fn-update.sh
+++ fn-update.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-# 書式・ファイル名などを綺麗にし、サイドバーを再生成する。
+# ファイル・ディレクトリの連番を綺麗にし、サイドバーを再生成する。
 # 第1引数として、処理対象のディレクトリを指定できる。（省略時は../docsディレクトリ）
 
 # 実行例
-# scripts/reformat.sh
-# scripts/reformat.sh docs/01_Linux
+# scripts/fn-update.sh
+# scripts/fn-update.sh docs/01_Linux
 
 docs_dir=$(dirname "$0")/docs
 target_dir="${1:-$docs_dir}"
 
-# 書式を綺麗にする。（lintに引っかからないように、先にh1を修正する）
+# 連番を綺麗にする。
 fn-update "$target_dir"
-md-h1 -f "$target_dir"
-markdownlint -f "$target_dir"
 
 # サイドバーを再生成する。
 "$(dirname "$0")/update-sidebar.sh"
